9f5bf095da3c61852f63a547e31bd2da9145e8b9
Mikel Sagardia <mxagar@gmail.com>
2022-06-25 11:47:43 +0200

    EDA commands

--- README.md
+++ README.md
@@ -40,29 +40,42 @@ For more information on the motivation of the guide, see my [blog post](https://
 - Use `np.log1p()` in case you have `x=0`; `log1p(x) = log(x+1)`
 - Use `df.apply()`!
 - Make a copy of the dataset if we drop or change variables: `data = df.copy()`.
+- Categorical variables must be enconded as quantitative variables.
+- Seaborn plots get `plt.figure(figsize=(10,10))` beforehand; pandas plots get `figsize` are argument.
+- `plt.show()` only on scripts!
+- Use a seed whenever there is a random number generation to ensure reproducibility!
+- Pandas slicing:
+	- `df[]` should access only to column names/labels: `df['col_name']`.
+	- `df.iloc[]` can access only to row & column index numbers + booleans: `df.iloc[0,'col_name']`, `df.iloc[:,'col_name']`.
+	- `df.loc[]` can access only to row & column labels/names + booleans: `df.loc['a','col_name']`, `df.loc[:,'col_name']`.
+	- `df[]` can be used for changing entire column values, but `df.loc[]` or `df.iloc[]` should be used for changing sliced row values.
 
 ## Data Cleaning
 
 - Always do general checks: `head()`, `info()`, `describe()`, `shape`.
 - Always get lists of column types: `select_dtypes()`.
-	- Categorical columns, `object`: `unique()`, `value_counts()`.
+	- Categorical columns, `object`: `unique()`, `value_counts()`. Can be subdivided in:
+		- `str`: text or category level.
+		- `datetime`: encode with `to_datetime()`.
 	- Numerical columns, `int`, `float`: `describe()`.
 - Detect and remove duplicates: `duplicated()`, `drop_duplicates()`.
 - Correct inconsistent text/typos in labels, use clear names: `replace()`, `map()`.
 - Detect and fix missing data: 
-	- `dropna()` rows iff several fields missing or missing field is key (e.g., target).
+	- Plot the missing amounts: `df.isnull().sum().sort_values(ascending=False)[:10].plot(kind='bar')`.
+	- `dropna()` rows if several fields missing or missing field is key (e.g., target).
 	- `drop()` columns if many (> 20-30%) values are missing. 
-	- Impute the missing field/column with `fillna()`: `mean()`, `meadian()`, `mode()`.
+	- Impute the missing field/column with `fillna()` if few (< 10%) rows missing: `mean()`, `meadian()`, `mode()`.
 	- Mask the data: create a category for missing values, in case it leads to insights.
 	- More advanced:
 		- Predict values with a model.
 		- Use k-NN to impute values of similar data-points.
 - Detect and handle outliers:
+	- Keep in mind the empricial rule of 68-95-99.7 (1-2-3 std. dev.).
+	- Compute `stats.zscore()` to check how many std. deviations from the mean; often Z > 3 is considered an outlier.
 	- Histogram plots: `sns.histplot()`.
 	- Box plots: `sns.boxplot()`.
 	- Scatterplots: `plt.scatter()`, `plt.plot()`.
 	- Residual plots: differences between the real/actual target values and the model predictions.
-	- Compute `stats.zscore()` to check how many std. deviations from the mean; often Z > 3 is considered an outlier.
 	- IQR calculation: use `np.percentile()`.
 	- Drop outliers? Only iff we think they are not representative.
 	- Do transformations fix the `skew()`? `np.log()`, `np.sqrt()`, `stats.boxcox()`.
@@ -70,31 +83,70 @@ For more information on the motivation of the guide, see my [blog post](https://
 
 ## Exploratory Data Analysis
 
+- Recall we have 3 main ways of plotting:
+	- Matplotlib: default: `plt.hist()`.
+	- Seaborn: nicer, higher interface: `sns.histplot()`.
+	- Pandas built-in: practical: `df.plot(kind='hist')`, `df.hist()`, `df.plot.hist()`.
+- We need to plot / observe **every** feature or variable:
+	- Create automatic lists of variable types: `select_dtypes()`. Usual types:
+		- Numerical: `int`, `float`.
+		- Strings: `object`. Can contain:
+			- `strings`: text or category level.
+			- `dates`: encode with `to_datetime()`.
+	- Loop each type list and apply the plots/tools we require.
+- Quantitative/numerical variables: can be uni/multi-variate; most common EDA tools:
+	- Histograms: `sns.histplot()`, `plt.hist()`, `df.hist()` - look at: shape, center, spread, outliers.
+	- Numerical summaries: `describe()`.
+	- Boxplots: `sns.boxplot()` - look at outliers.
+		- Also, combine these two: `sns.catplot()`, `sns.stripplot()`.
+	- Scatteplots: `sns.scatterplot()`, `plt.scatter()`, `sns.regplot()`, `sns.lmplot()` - look at: linear/quadratic relationship, positive/negative, strength: weak/moderate/strong.
+		- Beware of the [Simpson's Paradox](https://en.wikipedia.org/wiki/Simpson%27s_paradox).
+	- Correlations: `df.corr()`,  `stats.pearsonr()`; see below.
+		- Beware of the [Simpson's Paradox](https://en.wikipedia.org/wiki/Simpson%27s_paradox).
+- Categorical variables: ordinal (groups have ranking) / cardinal (no order in groups); most common EDA tools:
+	- Count values: `unique()`, `value_counts().plot(kind='bar')`.
+	- Frequency tables; see below.
+	- Bar charts: `sns.barplot()`, `plt.bar()`, `plt.barh()`; use `sort_values()`.
+	- Count plots: `sns.countplot()`.
+- If a continuous variable has different behaviors in different ranges, consider stratifying it with `pd.cut()`. Example: age -> age groups.
+- Frequency tables: stratify if necessary, group by categorical levels, count and compute frequencies.
 - Correlations:
 	- Heatmap for all numerical variables: `sns.heatmap(df.corr())`.
 	- Bar chart for correlations wrt. target: `df.corr()['target'].sort_values(ascending=True).plot(kind='bar')`.
+	- Pair correlations: `stats.pearsonr(df['x'],df['y'])`.
 	- Check if there is multicolinearity: it's not good.
+	- Beware of confounding: two correlated variables can be affected/related by something different.
+- If you want to try different or plots:
+	- Historgrams: add density, `kde=True` in `sns.histplot()`.
+	- Boxplots -> try `sns.swarmplot()`: boxes are replaced by point swarms.
+	- Boxplots -> try `sns.violinplot()`: box width changed.
+	- Scatterplots -> try `sns.lmplot()` or `sns.reglot()`: linear regression is added to the scatterplot.
+	- Scatterplots -> try `sns.jointplot()`: density distribution isolines of a pair of quantitative variables.
+- We can get a handle of any plot and set properties to is later: `bp = sns.boxplot(); bp.set_xlabel()`.
+- Larger group plots:
+	- `sns.pairplot()`: scatterplots/histograms of quantitative varaibles in a matrix; select variables if many.
+	- `sns.FacetGrid()`: create a grid according to classes and map plot types and variables.
 
 
 ## Feature Engineering
 
+- Always make a copy of the dataset if we change it: `df.copy()`.
 - Transformations: if variables have a `skew()` larger than 0.75.
 	- Target: usually the logarithm is applied: `df[col] = df[col].apply(np.log1p)`.
 		- That makes undoing the transformation very easy: `np.exp(pred)`.
 		- If power transformations used (e.g., `boxcox`, `yeojohnson`), we need to save the params/transformer and make sure we know how to invert the transformation!
-	- Predictor(independent) variables: `scipy` or `sklearn` can be used for power transformations (e.g., `boxcox`, `yeojohnson`). 
+	- Predictor /independent variables: `scipy` or `sklearn` can be used for power transformations (e.g., `boxcox`, `yeojohnson`). 
 - Extract / create new features, more descriptive
 	- Multiply different features if we suspect there might be an interaction
 	- Divide different features, if the division has a meaning.
-	- Create categorical data from continuous it that has a meaning.
+	- Create categorical data from continuous it that has a meaning, e.g., daytime.
 	- Try polynomial features: `PolynomialFeatures()`.
 	- Create deviation factors from the mean of a numeric variable in groups or categories of another categorical variable. 
-
-- Replace categorical levels with few counts with `'other'`
-
+- Replace categorical levels with few counts with `'other'`.
 - Feature encoding
-	- One-hot endoding / dummy variables: `get_dummies()`.
-- Feature scaling
+	- One-hot encoding / dummy variables: `get_dummies()`.
+- Train/test split: perform it before scaling the variables: `train_test_split()`.
+- Feature scaling: apply it if data-point distances are used in the model; fit the scaler only with the train split!
 	- `StandardScaler()`: subtract the mean and divide by the standard deviation; features are converted to standard normal viariables.
 	- `MinMaxScaler()`: a mapping with which the minimum value becomes 0, max becomes 1. This is senstive to outliers!
 	- `RobustScaler()`: IQR range is mapped to `[0,1]`, i.e., percentiles `25%, 75%`; thus, the scaled values go out from the `[0,1]` range.
@@ -102,7 +154,7 @@ For more information on the motivation of the guide, see my [blog post](https://
 ## Feature Selection
 
 - We can measure sparsity of information with `PCA()`; if less variables explain most of the variance, we could drop some.
-- Select variables with L1 regularized regression (lasso): `SelectFromModel(Lasso())`.
+- Select variables with L1 regularized regression (lasso): `SelectFromModel(Lasso())`. L1 regularization forces coefficients of less important variables to become 0; thus, we can remove them.
 - Use pairplots to check multi-colinearity; correlated features are not good.
 
 ## Data Modelling
--- data_processing.py
+++ data_processing.py
@@ -2,7 +2,22 @@
 The file won't run! It is a compilation of important code pieces ready
 for copy & paste.
 
+See the companion README.md with schematic explanations.
+
 Table of contents:
+
+- Imports
+- General, Useful & Important Functions
+- Data Cleaning
+- Exploratory Data Analysis (EDA)
+- Feature Engineering
+- Feature Selection
+- Inferences & Hypothesis Testings
+- Data Modelling
+
+Author: Mikel Sagardia.
+Date: 2016 - 2022.
+Repository: https://github.com/mxagar/eda_fe_summary
 
 '''
 
@@ -10,21 +25,34 @@ Table of contents:
 ### -- Imports
 ### -- 
 
+# Choose the imports that are necessary
+
 import datetime as dt
-import joblib # save python objects
+import joblib # save python objects (warning: python version specific)
 
 import pandas as pd
 import numpy as np 
 from scipy.stats import stats
+import scipy.stats.distributions as dist
 
 import seaborn as sns 
 import matplotlib.pylab as plt
 %matplotlib inline
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PowerTransformer
+from sklearn.decomposition import PCA
+
+# Binary or one-hot encoding with Scikit-Learn
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer, OneHotEncoder
+#from pandas import get_dummies
+
+# Ordinal encoding with Scikit-Learn; ordinal = there is a ranking order between category levels
+# Beware: we assume distances between categories are as encoded!
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import OrdinalEncoder
 
 # Lasso regression is the model used (L1 regularization: penaly to variable coefficients)
 # SelectFromModel identifies the features that are important for the model
@@ -35,7 +63,7 @@ from sklearn.feature_selection import SelectFromModel
 pd.pandas.set_option('display.max_columns', None)
 
 ##### -- 
-##### -- Useful Functions
+##### -- General, Useful & Important Functions
 ##### -- 
 
 df = pd.concat([X,y],axis=1)
@@ -44,7 +72,7 @@ df = pd.read_csv('data/dataset.csv')
 
 df.head(3)
 df.info()
-df["price"].describe()
+df["price"].describe() # use .T if many columns
 
 # Always save lists of numerical/continuous and categorical columns
 categorical_cols = list(df.select_dtypes(include = ['object']))
@@ -56,12 +84,16 @@ q25, q50, q75 = np.percentile(df['price'], [25, 50, 75])
 df['price'].skew()
 
 # Get uniques and counts of the levels of a categorcial variable
-df["condition"].value_counts()
+df["condition"].value_counts().sort_values(ascending=False).plot(kind='bar')
 df["condition"].unique()
 
 # Do you need sampling?
 # Pandas sample of n=5 rows/data-points that only appear once
 sample = df.sample(n=5, replace=False)
+
+# Basic text processing
+word_list = text.lower().split(' ')
+number = int(text.split(' ')[0])
 
 # GROUP BY
 # When grouping by a column/field,
@@ -75,7 +107,35 @@ df['date'] = pd.to_datetime(df['date'], format='%b-%y')
 df['month'] = df['date'].dt.month_name().str.slice(stop=3)
 df['year'] = df['date'].dt.year
 
-# Multiple filtering
+# Pandas slicing:
+# - `df[]` should access only to column names/labels: `df['col_name']`.
+# - `df.iloc[]` can access only to row & column index numbers + booleans:
+#		`df.iloc[0,'col_name']`, `df.iloc[:,'col_name']`.
+# - `df.loc[]` can access only to row & column labels/names + booleans:
+#		`df.loc['a','col_name']`, `df.loc[:,'col_name']`.
+# - `df[]` can be used for changing entire column values,
+#		but `df.loc[]` or `df.iloc[]` should be used for changing sliced row values.
+df.loc[['a', 'b'],'BMXLEG'] # df.index = ['a', 'b', ...]
+df.loc[[0, 1],3]
+
+# Selection / Filtering: Column selection after name
+col_names = df.columns # 'BMXWT', 'BMXHT', 'BMXBMI', 'BMXLEG', 'BMXARML', ...
+keep = [column for column in col_names if 'BMX' in column]
+df_BMX = df[keep]
+
+# Selection / Filtering: With booleans
+df.loc[:, keep].head()
+# # Indexing with booleans: Which column (names) are in keep?
+index_bool = np.isin(df.columns, keep)
+df_BMX = df.iloc[:,index_bool]
+
+# Multiple filtering: Several conditions
+waist_median = df_BMX['BMXWAIST'].median()
+condition1 = df_BMX['BMXWAIST'] > waist_median
+condition2 = df_BMX['BMXLEG'] < 32
+df_BMX[condition1 & condition2].head()
+
+# Multiple filtering: Another example
 df_filtered = df[(df['location'] == "Munich, Germany") | (df['location'] == "London, England")]
 cities = ['Munich', 'London', 'Madrid']
 df_filtered = df[df.location.isin(cities)]
@@ -96,16 +156,16 @@ df.index.is_unique
 # Detect missing values, sort them ascending, plot
 # isnull() == isna()
 total = df.isnull().sum().sort_values(ascending=False)
-total_select = total.head(20)
+total_select = total.head(20)/df.shape[0]
 total_select.plot(kind="bar", figsize = (8,6), fontsize = 10)
 plt.xlabel("Columns", fontsize = 20)
 plt.ylabel("Count", fontsize = 20)
 plt.title("Total Missing Values", fontsize = 20)
 
-# All rows with NA in column "Variable" dropped, but not inplace!
+# All rows with NA in column "variable" dropped, but not inplace!
 df.dropna(subset=["variable"])
 
-# Entire column "Lot Frontage" dropped, but not inplace!
+# Entire column "variable" dropped, but not inplace!
 df.drop("variable", axis=1)
 
 # Compute median of a column/feature
@@ -131,6 +191,9 @@ df = df.rename(columns={'fam': 'family'})
 df['gender'] = df['gender'].replace({'male':1, 'female':0})
 df['gender'].replace({'male':1, 'female':0}, inplace=True)
 
+# Detect a word and encode
+df['bath_private'] = df['bath_description'].apply(lambda text: 1 if 'private' in text else 0)
+
 # Map the values == Replace
 dictionary = {'value1':'val1', 'value3':'val2'}
 df['variable'] = df['variable'].map(dictionary)
@@ -142,7 +205,7 @@ for col in dat_cols:
     df[col] = df[col].apply(lambda col: int((today-col).days))
 
 ##### -- 
-##### -- Exploratory Data Analysis
+##### -- Exploratory Data Analysis (EDA)
 ##### -- 
 
 # Matplotlib: Several diagrams overlaid / Scatterplots if linestyle ''
@@ -180,32 +243,83 @@ plt.xlabel('Price, USD')
 plt.title('Price distribution depending on access to beach')
 plt.savefig('./pics/price_distribution_beach.png', dpi=300, transparent=False, bbox_inches='tight')
 
-# Bars, Horizontal bars
-plt.bar(np.arange(10), df.var.iloc[:10])
-plt.barh(np.arange(10), df.var.iloc[:10])
-
 # Scatterplots
 df.plot.scatter(x='var1', y='var2')
 plt.scatter(x=df['var1'], y=df['var2'])
+sns.scatterplot(x="seniority",y="income",hue="gender",data=df)
+
+# Jointplots: density isolines for a pair of quantitative variables
+# Two variables plotted; type of scatterplot scecified + density histograms
+sns.jointplot(x=df['var1'],y=df['var2'],kind='hex')
+
+# Scatterplot -> Scatterplot + Linear regression line plotted
+sns.lmplot(x='x', y='y', data=df)
+sns.reglot(x='x', y='y', data=df)
+# ... with hue
+sns.lmplot(x='x', y='y', data=df,
+           fit_reg=False, # No regression line
+           hue='category') # Color by category group
 
 # Boxplots
 df.boxplot(by='category');
+sns.boxplot(x="species", y="height", data=df)
+sns.boxplot(data=df.loc[:, ["A", "B", "C", "D"]]) # just the boxes of A, B, C, D
+
+# Boxplots: Nicer - points displayed
+for var in categorcial_selected:
+	# Boxplot
+    sns.catplot(x=var, y='price', data=df, kind="box", height=4, aspect=1.5)
+    # Data points
+    sns.stripplot(x=var, y='price', data=df, jitter=0.1, alpha=0.3, color='k')
+
+# Boxplots: stratify + apply hue
+df["age_group"] = pd.cut(df.age, [18, 30, 40, 50, 60, 70, 80])
+sns.boxplot(x="age_group", y="blood_pressure", hue="gender", data=df)
+
+# Swarmplot (<- Boxplots): point swarms instead of boxes
+sns.swarmplot(x="x", y="y", data=df)
+
+# Violin plots (<- Boxplots): A box plot with varying box width
+sns.violinplot(x="gender", y="age", data=df)
 
 # Correlations and heatmaps
-sns.heatmap(new_data.corr(),annot=True,cmap='RdYlGn')
+sns.heatmap(df.corr(),annot=True,cmap='RdYlGn')
+df.corr()['target'].sort_values(ascending=True).plot(kind='bar')
 
+# Frequency tables: Stratify & Group
+df["age_group"] = pd.cut(df.age, [18, 30, 40, 50, 60, 70, 80])
+# Eliminate rare/missing values
+dx = df.loc[~df.education_level.isin(["Don't know", "Missing"]), :] 
+dx = dx.groupby(["age_group", "gender"])["education_level"]
+dx = dx.value_counts()
+# Restructure the results from 'long' to 'wide'
+dx = dx.unstack()
+# Normalize within each stratum to get proportions
+dx = dx.apply(lambda x: x/x.sum(), axis=1)
+# Limit display to 3 decimal places
+print(dx.to_string(float_format="%.3f"))
+# education_level        9-11    <9  College  HS/GED  Some college/AA
+# age_group   gender                                              
+# (18, 30] 	  Female    0.080 0.049    0.282   0.215            0.374
+#             Male      0.117 0.042    0.258   0.250            0.333
+# (30, 40]    Female    0.089 0.097    0.314   0.165            0.335
+# ...
 
-# PAIRPLOT
+# Bars, Horizontal bars
+plt.bar(np.arange(10), df.var.iloc[:10])
+plt.barh(np.arange(10), df.var.iloc[:10])
+sns.barplot(data=df,x='married',y='income',hue='gender')
+
+# Countplots: bar plot of the selected variable
+sns.countplot(x='gender', data=df)
+
+# Pairplots: scatterplot/histogram matrix
 # All variables plotted against each other: scatterplots, histograms
 # If many variables, select the ones we're interested in
 # Hue: separate/group by categories
 sns.pairplot(df[selected], hue='species', size=3)
 
-# JOINTPLOT
-# Two variables plotted; type of scatterplot scecified + density histograms
-sns.jointplot(x=df['var1'],y=df['var2'],kind='hex')
-
-# FACETGRID: map plot types to a grid of plots
+# FacetGrid: map plot types to a grid of plots
 # 1: specify dataset and col groups/levels
 plot = sns.FacetGrid(df, col='species', margin_titles=True)
 # 2: which variable to plot in cols, and which plot type (hist)
@@ -219,8 +333,25 @@ plot.map(plt.hist, 'height', color='green')
 # Always make a copy!
 df = data.copy()
 
+# Statsmodels: Yeo-Johnson and Box-Cox transormations
+# Always store params!
+df[var+'_box'], param_box = stats.boxcox(df[var])
+df[var+'_yeo'], param_yeo = stats.yeojohnson(df[var])
+# We can do it in a for-loop for selected variables + visualize changes
+for var in selected_cols:
+    tmp[var], param = stats.yeojohnson(df[var])    
+    plt.subplot(1, 2, 1)
+    plt.scatter(df[var], df['target'])
+    plt.ylabel('Target')
+    plt.xlabel('Original ' + var)
+    plt.subplot(1, 2, 2)
+    plt.scatter(tmp[var], df['target'])
+    plt.ylabel('Target')
+    plt.xlabel('Transformed ' + var)
 
-boxcox_transformed = pd.Series(stats.boxcox(df['SalePrice'])[0])
+# Scikit-Learn Power Transformer
+pt = PowerTransformer(method='box-cox', standardize=False) # method=‘yeo-johnson’
+df['var_transformed'] = pt.fit_transform(df['var'])
 
 # Multiplicative interaction
 df['v1_x_v2'] = df['v1'] * df['v2']
@@ -253,8 +384,7 @@ def add_deviation_feature(df, feature, category):
     df[feature + '_dev_' + category] = deviation_feature 
 add_deviation_feature(df, 'overall_quality', 'neighborhood')
 
-
-# Dummies with pandas / One-hot encoding
+# One-hot encoding: Dummy variables with pandas
 col_dummies = ['var1', 'var2']
 try:
     for col in col_dummies:
@@ -265,7 +395,6 @@ try:
 except KeyError as err:
     print("Columns already dummified!")
 
-
 # Train/Test split
 y = df['price']
 X = df.drop('price',axis=1)
@@ -275,7 +404,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.1, # portion of dataset to allocate to test set
     random_state=42, # we are setting the seed here
 )
-
 
 # Fit the scaler to the train set only
 scaler = MinMaxScaler()
@@ -301,6 +429,22 @@ joblib.dump(scaler, filepath+'minmax_scaler.joblib')
 ##### -- 
 
 # Check sparsity with PCA
+scaler = StandardScaler()
+X = scaler.fit_transform(X.astype(np.float64))
+# PCA with all components
+pca = PCA(n_components = X.shape[1])
+pca.fit_transform(x)
+# Explained variance of each principal component
+explained_variance = pca.explained_variance_ratio_
+# Sum all explained variances until 95% is reached;
+# how many components do we need?
+cumsum = np.cumsum(pca.explained_variance_ratio_)
+d = np.argmax(cumsum >=0.95) + 1
+# Another way to answer the question above:
+# pass varinace ratio float as n_components
+pca = PCA(n_components=0.95)
+X_reduced = pca.fit_transform(X)
+X_reduced.shape[1]
 
 
 
@@ -317,6 +461,111 @@ joblib.dump(selected_features, filepath+'selected_features.joblib')
 ##### -- 
 ##### -- Inferences & Hypothesis Testings
 ##### -- 
+
+def z_test(subset1, subset2):
+    '''This function computes the Z Test of two population proportions.
+    H0: The proportions are the same.
+    Ha: The proportions are different (two-sided).
+    
+    Input:
+        subset1: data frame with values to analyze for subset/group 1
+        subset1: data frame with values to analyze for subset/group 2
+    Output:
+        z_stat: Z statistic; if very negative, subset1 has a considerebly smaller proportion
+        p_value: if < 0.05, the difference is significant, the two proportions are different
+    '''
+    # Sample sizes
+    n1 = subset1.shape[0]
+    n2 = subset2.shape[0]
+    
+    # Number of positive values
+    y1 = subset1.sum()
+    y2 = subset2.sum()
+
+    # Estimates of the population proportions
+    p1 = np.round(y1 / n1, 2)
+    p2 = np.round(y2 / n2, 2)
+    
+    if p1 == p2:
+        print('Warning: same proportions!')
+
+    # Estimate of the combined population proportion
+    phat = (y1 + y2) / (n1 + n2)
+
+    # Estimate of the variance of the combined population proportion
+    va = phat * (1 - phat)
+
+    # Estimate of the standard error of the combined population proportion
+    se = np.sqrt(va * ((1.0 / n1) + (1.0 / n2)))
+
+    # Test statistic and its p-value: 2-sided, because the Ha is !=
+    z_stat = (p1 - p2) / se
+    p_value = 2*dist.norm.cdf(-np.abs(z_stat))
+    # Equivalent to
+    # p_value = 2*(1-dist.norm.cdf(np.abs(z_stat)))
+
+    return (z_stat, p_value, p1, p2)
+
+# Apply Z Test to binary variables (i.e., proportions of groups)
+group1 = 'Donostia'
+group2 = 'Bilbao'
+feature_analyze = 'bathrooms_shared'
+subset1 = df[df[group1] > 0][feature_analyze]
+subset2 = df[df[group2] > 0][feature_analyze]
+(z_stat, p_value, p1, p2) = z_test(subset1, subset2)
+
+def t_test(subset1, subset2):
+    '''This function computes the Z Test of two population proportions.
+    H0: The proportions are the same.
+    Ha: The proportions are different (two-sided).
+
+    Input:
+        subset1: data frame with values to analyze for subset/group 1
+        subset1: data frame with values to analyze for subset/group 2
+    Output:
+        t_stat: T statistic; if very negative, subset1 has a considerebly smaller mean
+        p_value: if < 0.05, the difference is significant, the two distribution means are different
+    '''
+    
+    # Sample sizes    
+    n1 = subset1.shape[0]
+    n2 = subset2.shape[0]
+    
+    # Means
+    m1 = subset1.mean()
+    m2 = subset2.mean()
+
+    # Standard Deviations
+    sd1 = subset1.std()
+    sd2 = subset2.std()
+
+    # Standard Error
+    if sd1/sd2 < 2.0 or sd2/sd1 < 2.0:
+        # Similar variances: Pooled
+        se = np.sqrt(((sd1**2)/n1) + ((sd2**2)/n2))
+    else:
+        # Different variances: Unpooled
+        se = np.sqrt(((n1-1)*sd1**2 + (n2-1)*sd2**2)/(n1+n2-2)) * np.sqrt((1/n1) + (1/n2))
+    
+    # T statistic
+    t_stat = (m1 - m2)/se    
+    # With T and df, we can get the p-value from the T distribution
+    df = n1 + n2 - 2
+    # p-value is obtained form the cummulative density function (CDF) with the given df
+    # since we want the remaining are under the PDF, we need to compute 1-CDF(t)
+    # Since it is a 2-sided test (Ha !=), we need to x2 the p-value
+    p_value = 2*(1 - dist.t.cdf(np.abs(t_stat),df=df))
+    
+    return (t_stat, p_value, m1, m2)
+
+# Apply T Test to continuous variables (means of groups)
+group1 = 'has_beach' # == 1
+group2 = 'has_beach' # == 0
+feature_analyze = 'price'
+subset1 = df[df[group1] > 0][feature_analyze]
+subset2 = df[df[group2] < 1][feature_analyze]
+(t_stat, p_value, m1, m2) = t_test(subset1, subset2)
+
 
 ##### -- 
 ##### -- Data Modelling
